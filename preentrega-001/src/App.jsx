a76c2dd4a221336f6b08f9c45cf0b72ccf2f1af0
Jim Reyna <rreyna1982@gmail.com>
2023-01-23 23:38:57 -0500

    carga proyecto

---
+++ preentrega-001/src/App.jsx
@@ -0,0 +1,14 @@
+import React from './componentes/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './componentes/NavBar/NavBar';
+
+function App() {
+
+  return (
+    
+      <NavBar/>
+  
+  )
+}
+
+export default App
